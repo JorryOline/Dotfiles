ee073ca3957e7d3543d582373f5130dc1fa181f1
JorryOline <19022172890@163.com>
2025-07-25 08:02:17 +0800

    更改了neovide配置

--- nvim/init.lua
+++ nvim/init.lua
@@ -54,7 +54,7 @@ local alpha = function()
 end
 -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
 vim.g.neovide_opacity = 0.9
-vim.g.transparency = 1.0
+vim.g.transparency = 0.9
 --vim.g.neovide_background_color = "#0f1117" .. alpha()
 
     --vim.o.guifont = "JetBrainsMono Nerd Font Mono Bold:h14" -- text below applies for VimScript
