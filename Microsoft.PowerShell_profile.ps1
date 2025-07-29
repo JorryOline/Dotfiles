1fb371ca19deb759954752043e42043770fa52a4
JorryOline <19022172890@163.com>
2025-07-29 08:57:09 +0800

    update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,25 +1,29 @@
+#Set-PSReadlineKeyHandler -ChordTab -FunctionMenuComplete
+Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView
 # function DailySaying {
 #     echo "AT fi-rudo Zenkai!" | tte --xterm-colors --wrap-text beams
 # }
 #Set-Alias daysay DailySaying
 #~/main.ps1
 $env:POWERSHELL_UPDATECHECK = 'Off'
-function s { fastfetch --config os }
 Set-Alias cl clear
 Set-Alias vim nvim
-Set-Alias s Fastfetch
+Set-Alias lg lazygit
+# Set-Alias s Fastfetch
 Set-Alias m musicfox
 #Set-Alias pj 'cd E:\project\'
-function Fastfetch {
-  fastfetch --config minimal
-}
-
+# function Fastfetch {
+#   fastfetch --config minimal
+# }
+Set-Alias lvim C:\Users\Administrator\.local\bin\lvim.ps1
 function GoToProject {
     cd "e:/project"
 }
 Set-Alias pj GoToProject
 # Set-Alias pj 'cd`e:/project/'
 Set-Alias z zoxide.exe
+
+# 这里可以放置你原本在配置文件中的其他命令
 
 
 #oh-my-posh init pwsh | Invoke-Expression
@@ -34,9 +38,6 @@ function NvimandFzf {
 }
 Set-Alias vim NvimandFzf
 
-
-
-
-
 Invoke-Expression (&starship init powershell)
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
